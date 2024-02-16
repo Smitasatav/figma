ffe0cb21e1f7676068a3a56b0665d88aadd526c5
Smita Satav <smitasatav0134@gmail.com>
2024-02-16 15:49:12 +0530

    intro to bootstrap

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,104 +1,77 @@
 "use client";
-import { useState, useEffect } from "react";
-import Link from "next/link";
-import Navbar from "@/components/Navbar";
-import { taskDef } from "@/components/types";
-import Spinner from "@/components/Spinner";
-import axios from "@/components/api";
 
 export default function Home() {
-  const [tasks, setTasks] = useState<taskDef[]>([]);
-  const [loading, setLoading] = useState(false);
-
-  const fetchData = async () => {
-    setLoading(true);
-    try {
-      const res = await axios.get("/tasks");
-      setTasks(res.data.items);
-    } catch (error) {
-      console.error("Error fetching tasks:", error);
-    }
-    setLoading(false);
-  };
-
-  const deleteTask = async (task: taskDef) => {
-    if (task.scheduled) {
-      alert(`this task ${task.title} cannot be deleted`);
-      return;
-    }
-    try {
-      setLoading(true);
-      await axios.delete(`/tasks/${task._uuid}`);
-      fetchData();
-    } catch (error) {
-      console.error("Error deleting task:", error);
-    }
-    setLoading(false);
-  };
-
-  const scheduledTask = async (task: taskDef) => {
-    setLoading(true);
-    await axios.put(`/tasks/${task._uuid}`, { scheduled: !task.scheduled });
-    await fetchData();
-    setLoading(false);
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
   return (
     <main>
-      <Navbar title="Todo App" rightIcon="calender" />
-      {loading && <Spinner />}
-      {!loading && (
-        <div className="todo-center-container">
-          <div className="todo-list-container">
-            {tasks.map((task) => (
-              <div className="todo-bar" key={task.title}>
-                <div className="todo-bar-left-section">
-                  <div className="title">{task.title}</div>
-                  <div className="sub-title">{task.detail}</div>
-                </div>
-                <div className="todo-bar-right-section">
-                  <Link href={`/edit-task/${task._uuid}`}>
-                    <img src="./icons/pencil.svg" />
-                  </Link>
-                  <img
-                    src="./icons/trash.svg"
-                    onClick={() => deleteTask(task)}
-                  />
-                  <img
-                    src="./icons/check-circle.svg"
-                    onClick={() => scheduledTask(task)}
-                    className={task.scheduled ? "scheduled" : ""}
-                  />
-                </div>
-              </div>
-            ))}
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">First</th>
+            <th scope="col">Last</th>
+            <th scope="col">Handle</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <th scope="row">3</th>
+            <td colSpan={2}>Larry the Bird</td>
+            <td>@twitter</td>
+          </tr>
+        </tbody>
+      </table>
 
-            <div className="add-todo-button">
-              <Link href="/add-task" className="button">
-                +
-              </Link>
-            </div>
+      <form>
+        <div className="mb-3">
+          <label htmlFor="exampleInputEmail1" className="form-label">
+            Email address
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+          />
+          <div id="emailHelp" className="form-text">
+            We'll never share your email with anyone else.
           </div>
         </div>
-      )}
-      <div className="footer-container">
-        <div className="left-footer">
-          <div>
-            <img src="./icons/vector.svg" />
-          </div>
-          <div className="footer-text">All</div>
+        <div className="mb-3">
+          <label htmlFor="exampleInputPassword1" className="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            id="exampleInputPassword1"
+          />
         </div>
-        <div className="right-footer">
-          <div>
-            <img src="./icons/tick.svg" />
-          </div>
-          <div className="footer-text">Completed</div>
+        <div className="mb-3 form-check">
+          <input
+            type="checkbox"
+            className="form-check-input"
+            id="exampleCheck1"
+          />
+          <label className="form-check-label" htmlFor="exampleCheck1">
+            Check me out
+          </label>
         </div>
-      </div>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
     </main>
   );
 }
