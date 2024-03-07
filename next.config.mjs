2e608dc37e150f5cf735365f7b15e64e70db7282
Ayushi0612 <124232340+Ayushi0612@users.noreply.github.com>
2024-03-07 18:35:56 +0530

    i18n used

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,10 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-   debug: process.env.NODE_ENV === 'development',
-    i18n: {
-      locales: ['en', 'es'],
-      defaultLocale: 'en',
-    },
+  i18n: {
+    locales: ['en', 'es'],
+    defaultLocale: 'en',
+  },
 };
 
 export default nextConfig;
